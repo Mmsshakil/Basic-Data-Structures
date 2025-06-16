b1360dd86596f12a1e4373e57bce1f6d53baabde
Mmsshakil <softhandshakil@gmail.com>
2025-06-17 03:25:40 +0600

    first commit

---
+++ Module_01_Time_and_Space_complexity/big_O_n_complexity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;    // complexity O(1)
+    cin >> n; // complexity O(1)
+
+    for (int i = 0; i < n; i++) // complexity O(n)
+    {
+        cout << i << endl;
+    }
+
+    return 0; // complexity O(1)
+}
+
+// in this code wrost case is O(n)
+// so complexity is O(n)
